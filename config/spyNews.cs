b24b549e776d86d24abc5e3d96a5f09ee5bd2b10
Drew Boyuka <drew.boyuka@gmail.com>
2016-03-30 18:37:55 -0400

    Checkin of config/ things for SpyMod

---
+++ config/spyNews.cs
@@ -0,0 +1,4 @@
+$News::numLines = 3;
+$News::line[0] = "\n\n\n<jc><f8>-- -- --   Recent SpyMod News   -- -- --\n\n";
+$News::line[1] = "<jl><l5><f7>April 10th - SpyMod news added!";
+$News::line[2] = "<jl><l10><f1>The SpyMod news feature has been added.  Check here for the latest SpyMod news and updates!";
